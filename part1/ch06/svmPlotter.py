cb14f4b54569314e5436f2d6523963d69a28bdca
teddy.ma <teddy.ma@ef.com>
2018-07-18 10:28:19 +0800

    complete ch06 svm

--- part1/ch06/svmPlotter.py
+++ part1/ch06/svmPlotter.py
@@ -6,10 +6,10 @@ Plotting svm Source Code for Machine Learning in Action Ch. 6
 
 from svm import *
 
-import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from numpy import *
+from sklearn.svm import SVC
 
 # get support vectors from smo output
 def getSVs(dataArr, labelArr, alphas):
@@ -52,18 +52,19 @@ def plotSmoSimple():
     
     plt.show()
 
-# 6.4 Figure 6.4 plot SmoP on sample dataset
-def plotSmoP(k1=1.3):
+def plotSklearnLinear():
     dataArr,labelArr = loadDataSet('testSet.txt')
-    b,alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1)) #C=200 important
-    svs,labels = getSVs(dataArr, labelArr, alphas)
-    w = calcWs(alphas, dataArr, labelArr)
+    clf = SVC(kernel='linear')
+    clf.fit(array(dataArr), array(labelArr))
+    b = array([clf.intercept_])
+    svs,labels = array(mat(dataArr)[list(clf.support_),:]),array(mat(labelArr)[:,list(clf.support_)])
+    w = clf.coef_[0]
     x = arange(-2.0, 12.0, 0.1)
     y = array((-w[0]*x - b)/w[1])[0]
     
     fig = plt.figure()
     ax = fig.add_subplot(111)   
-    #ax.axis([-2,12,-8,6])
+    ax.axis([-2,12,-8,6])
     
     # plot label 1 points
     i0 = nonzero(mat(labelArr).A == 1)[1] # get index of label 1 points
@@ -85,3 +86,55 @@ def plotSmoP(k1=1.3):
     
     plt.show()
 
+def plotSmoPRbf(k1=1.3):
+    dataArr,labelArr = loadDataSet('testSetRBF.txt')
+    b,alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1)) #C=200 important
+    svs,labels = getSVs(dataArr, labelArr, alphas)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)   
+    ax.axis([-2,2,-1,1.2])
+    
+    # plot label 1 points
+    i0 = nonzero(mat(labelArr).A == 1)[1] # get index of label 1 points
+    xy0 = array(dataArr)[i0,:]
+    ax.scatter(xy0[:,0],xy0[:,1], marker='o', s=30, c='red')
+    
+    # plot label -1 points
+    i1 = nonzero(mat(labelArr).A == -1)[1] # get index of label -1 points
+    xy1 = array(dataArr)[i1,:]
+    ax.scatter(xy1[:,0],xy1[:,1], marker='s', s=30)
+    
+    # plot support vectors
+    for pt in array(svs):
+        circle = Circle((pt[0]+0.005, pt[1]-0.01), 0.05, facecolor='none', edgecolor=(0,0.8,0.8), linewidth=2, alpha=0.5)
+        ax.add_patch(circle)
+    
+    plt.show()
+
+def plotSklearnRbf():
+    dataArr,labelArr = loadDataSet('testSetRBF.txt')
+    clf = SVC()
+    clf.fit(array(dataArr), array(labelArr))
+    svs,labels = array(mat(dataArr)[list(clf.support_),:]),array(mat(labelArr)[:,list(clf.support_)])
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)   
+    ax.axis([-2,2,-1,1.2])
+    
+    # plot label 1 points
+    i0 = nonzero(mat(labelArr).A == 1)[1] # get index of label 1 points
+    xy0 = array(dataArr)[i0,:]
+    ax.scatter(xy0[:,0],xy0[:,1], marker='o', s=30, c='red')
+    
+    # plot label -1 points
+    i1 = nonzero(mat(labelArr).A == -1)[1] # get index of label -1 points
+    xy1 = array(dataArr)[i1,:]
+    ax.scatter(xy1[:,0],xy1[:,1], marker='s', s=30)
+    
+    # plot support vectors
+    for pt in array(svs):
+        circle = Circle((pt[0]+0.005, pt[1]-0.01), 0.05, facecolor='none', edgecolor=(0,0.8,0.8), linewidth=2, alpha=0.5)
+        ax.add_patch(circle)
+    
+    plt.show()
